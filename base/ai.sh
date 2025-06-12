bcdc4c24f1350c5efdcbd6febf87e4279fd8425b
yuxuanyang <cyl01030810@163.com>
2025-06-12 11:18:54 +0000

    update ai shell
    Signed-off-by: yuxuanyang <cyl01030810@163.com>

--- base/ai.sh
+++ base/ai.sh
@@ -1,18 +1,13 @@
-if [ -n "$@" ];then
-echo $@
-#curl -X POST https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions -H "Authorization: Bearer sk-dfb4196dc2d14942acf3c9c1fb327f99" -H "Content-Type: application/json" -d '{
-#    "model": "qwen-plus",
-#    "messages": [
-#        {
-#            "role": "system",
-#            "content": "You are a helpful assistant."
-#        },
-#        {
-#            "role": "user",
-#            "content": "$@"
-#        }
-#    ]
-#}'
+#!/bin/bash
+
+model="qwen-plus"
+prompt="You are a helpful assistant."
+
+if  [ $# -ne 0 ];then
+   request="$*"
+   json_obj=$(printf '{"model": "%s", "messages": [{"role": "system","content": "%s"},{"role": "user","content": "%s"}]}' "$model" "$prompt" "$request")
+   #echo "$json_obj"
+   curl -X POST https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions -H "Authorization: Bearer sk-dfb4196dc2d14942acf3c9c1fb327f99" -H "Content-Type: application/json" -d "$json_obj"
 else
   echo 'input question invaild'
 fi
